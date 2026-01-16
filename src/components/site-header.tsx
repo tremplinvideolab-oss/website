e2c8e0c02faca900cae0413b0c1e7194a6d57e50
Aurélien Morel <ohms.morel@gmail.com>
2026-01-16 01:23:04 +0000

    on dirait que c'est l'icone que tu n'arrive pas a agrandir en fait.

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -81,9 +81,9 @@ export function SiteHeader() {
           <SheetTrigger asChild>
             <Button
               variant="ghost"
-              className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden h-16 w-16"
+              className="mr-2 h-16 w-16 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden [&_svg]:h-10 [&_svg]:w-10"
             >
-              <Menu className="h-16 w-16" />
+              <Menu />
               <span className="sr-only">{dict.siteHeader.toggleMenu}</span>
             </Button>
           </SheetTrigger>
